6d52facc4b9a4ae34e78d13e37ca8bcd078419bc
Sarah Krasnik <sarahmk125@gmail.com>
2020-05-28 21:18:53 -0400

    comma, and updating order

--- create_connectors.sh
+++ create_connectors.sh
@@ -1,5 +1,5 @@
-# Create sink connector
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d @sink_connector.json
-
 # Create source connector
 curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d @source_connector.json
+
+# Create sink connector
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d @sink_connector.json
